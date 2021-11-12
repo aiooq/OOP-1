41cf859c08ce03c8af52e074c76fa2fbf7db4681
aiooq <59270390+aiooq@users.noreply.github.com>
2021-11-12 16:40:09 +0700

    Update

--- program.py
+++ program.py
@@ -8,43 +8,43 @@ try:
     
     try:
         from tasks.task1 import Task as task1
-        tasks.append(task1()())
+        # tasks.append(task1()())
     except ImportError:
         pass
 
     try:
         from tasks.task2 import Task as task2
-        tasks.append(task2()())
+        # tasks.append(task2()())
     except ImportError:
         pass
 
     try:
         from tasks.task3 import Task as task3
-        tasks.append(task3()())
+        # tasks.append(task3()())
     except ImportError:
         pass
 
     try:
         from tasks.task4 import Task as task4
-        tasks.append(task4()())
+        # tasks.append(task4()())
     except ImportError:
         pass
 
     try:
         from tasks.task5 import Task as task5
-        tasks.append(task5()())
+        # tasks.append(task5()())
     except ImportError:
         pass
 
     try:
         from tasks.task6 import Task as task6
-        tasks.append(task6()())
+        # tasks.append(task6()())
     except ImportError:
         pass
 
     try:
         from tasks.task7 import Task as task7
-        tasks.append(task7()())
+        # tasks.append(task7()())
     except ImportError:
         pass    
 
